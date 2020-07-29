e001cdf597d1d29a61d518f606149e2aac6f3847
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-07-29 01:57:13 -0300

    Update github.strategy.js

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -9,7 +9,7 @@ passport.use(
         {
             clientID: "Iv1.eb54e1f5565b8535",
             clientSecret: "71c0f02f1c3220b60a6a145a545370af608c1eba",
-            callbackURL: "/auth/github/callback"
+            callbackURL: "https://nodejs-trabalhofinal-pwn.herokuapp.com/auth/github/callback"
         },
         function (accessToken, refreshToken, profile, done) {
             return done(undefined, profile);

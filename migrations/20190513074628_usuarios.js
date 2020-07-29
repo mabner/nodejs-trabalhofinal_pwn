985b3341d0c26a8cd35f65e81fc14ef668045f7a
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-07-29 01:36:16 -0300

    code opt

--- migrations/20190513074628_usuarios.js
+++ migrations/20190513074628_usuarios.js
@@ -1,14 +1,14 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('usuarios', table => {
-    table.increments('id_usr').primary()
-    table.timestamps('dt_usr')
-    table.string('e_mail')
-    table.string('nm_usr')
-    table.string('senha')
-    table.string('st_inativo')
-  })
+exports.up = function (knex) {
+    return knex.schema.createTable('usuarios', table => {
+        table.increments('id_usr').primary()
+        table.timestamps('dt_usr')
+        table.string('e_mail')
+        table.string('nm_usr')
+        table.string('senha')
+        table.string('st_inativo')
+    })
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('usuarios')
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('usuarios')
 };

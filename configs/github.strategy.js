f6fc701e23cd5361099c4623f1614fb62818b5ed
Marcos Leite <30819867+mabner@users.noreply.github.com>
2020-07-31 02:41:44 -0300

    Update github.strategy.js

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -8,16 +8,18 @@ passport.use(
 	new GitHubStrategy(
 		{
 			//** Locahost for testing */
-			clientID: 'Iv1.62b6860a6ffe024f',
-			clientSecret:
-				'0179e821aad5c2d51926d83e28bc69ab02a22090',
-			callbackURL:
-				'http://localhost:3000/auth/github/callback',
+			// clientID: 'Iv1.62b6860a6ffe024f',
+			// clientSecret:
+			// 	'0179e821aad5c2d51926d83e28bc69ab02a22090',
+			// callbackURL:
+			// 	'http://localhost:3000/auth/github/callback',
 
 			//** Online version - Change before final deploy */
-			// clientID: "Iv1.eb54e1f5565b8535",
-			// clientSecret: "71c0f02f1c3220b60a6a145a545370af608c1eba",
-			// callbackURL: "https://nodejs-trabalhofinal-pwn.herokuapp.com/auth/github/callback"
+			clientID: 'Iv1.eb54e1f5565b8535',
+			clientSecret:
+				'71c0f02f1c3220b60a6a145a545370af608c1eba',
+			callbackURL:
+				'https://nodejs-trabalhofinal-pwn.herokuapp.com/auth/github/callback',
 		},
 		function (accessToken, refreshToken, profile, done) {
 			return done(undefined, profile);

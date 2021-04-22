9566e7338ad8b8eefbe437cf4e13575bef51ba35
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-21 23:47:55 -0300

    Rovoked api keys and db password

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -9,12 +9,12 @@ passport.use(
 		{
 			//** Localhost for testing */
 			// clientID: 'Iv1.62b6860a6ffe024f',
-			// clientSecret: '0179e821aad5c2d51926d83e28bc69ab02a22090',
+			// clientSecret: '',
 			// callbackURL: 'http://localhost:3000/auth/github/callback',
 
 			//** Online version - Change before final deploy */
 			clientID: 'Iv1.eb54e1f5565b8535',
-			clientSecret: '71c0f02f1c3220b60a6a145a545370af608c1eba',
+			clientSecret: '',
 			callbackURL:
 				'https://nodejs-trabalhofinal-pwn.herokuapp.com/auth/github/callback',
 		},
--- store/Connection.js
+++ store/Connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.set('useUnifiedTopology', true);
 //MongoDB connection
 const connectionString =
-	'mongodb+srv://mrfishman:GACjfWg5FJD3Qj2@cluster0.060t3.gcp.mongodb.net/puc?retryWrites=true&w=majority';
+	'mongodb+srv://mrfishman:-@cluster0.060t3.gcp.mongodb.net/puc?retryWrites=true&w=majority';
 
 const openConnection = () =>
 	mongoose.connect(connectionString, { useNewUrlParser: true });
